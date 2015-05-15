96eaed3d980a65bf91594f33a94bafdea64ef751
Ashtryd <raquel.higong@alumnos.upm.es>
2015-05-15 12:15:45 +0200

    Marco aplicacion

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 //var users = require('./routes/users');
-
+var partials = require('express-partials');
 var app = express();
 
 // view engine setup
@@ -15,6 +15,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
+app.use(partials());
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
